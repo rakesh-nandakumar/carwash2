dbabc627c391ab2cfe9d9a6cca8de5cfe0dbe35c
Rakesh <rakeshnandakumar2005@gmail.com>
2026-09-20 22:06:28 +0530

    updates

--- resources/views/central/login.blade.php
+++ resources/views/central/login.blade.php
@@ -46,7 +46,7 @@
             </label>
             <button type="submit">Sign in</button>
         </form>
-        <div class="footer">Powered by - Vellix Global</div>
+        <div class="footer">Powered by Vellix Global</div>
     </div>
 </body>
 </html>
